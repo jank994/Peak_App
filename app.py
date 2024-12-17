9e84a39b0fcacbd57b4aafa7e2dee958a1322405
jank994 <jan.kokalj1@gmail.com>
2024-12-17 18:59:28 +0100

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import psycopg2
 
 app = Flask(__name__)
 
+
 # Povezava z bazo PostgreSQL
 def get_db_connection():
     conn = psycopg2.connect(
@@ -14,24 +15,25 @@ def get_db_connection():
     )
     return conn
 
+
 # API klici
 @app.route('/mountains', methods=['GET'])
 def get_mountains():
     conn = get_db_connection()
     cursor = conn.cursor()
-    
+
     cursor.execute('''SELECT * FROM mountains''')
     mountains = cursor.fetchall()  # Dobimo seznam
     cursor.close()
     conn.close()
-    
-    mountains_list = [] #ustvarimo slovar
+
+    mountains_list = []  # ustvarimo slovar
     for mountain in mountains:
         mountains_list.append({
             "mountain_id": mountain[0],
             "mountain": mountain[1],
-            "altitude": mountain[2], 
-            "country": mountain[3], 
+            "altitude": mountain[2],
+            "country": mountain[3],
             "lat": float(mountain[4]),
             "lon": float(mountain[5])
         })
@@ -44,12 +46,10 @@ def get_visited_mountains():
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute('''
-                    SELECT mountain, altitude, user_name
+                    SELECT mountain, altitude, country, datetime
                     FROM visited v
-                    INNER JOIN mountains mo ON v.mountain_id = mo.mountain_id
-                    INNER JOIN users u ON v.user_id = u.user_id   
                    ''')
-    visited = cursor.fetchall() #dobimo seznam
+    visited = cursor.fetchall()  # dobimo seznam
     cursor.close()
     conn.close()
 
@@ -58,28 +58,54 @@ def get_visited_mountains():
         visited_list.append({
             "mountain": v[0],
             "altitude": v[1],
-            "user_name": v[2]
+            "country": v[2],
+            "datetime": v[3],
         })
 
     return jsonify(visited_list)
+
+@app.route('/last_visited', methods=['GET'])
+def get_last_visited_mountains():
+    mountain = request.args.get('mountain')
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+                    SELECT mountain, datetime
+                    FROM visited v
+                    WHERE mountain = %s
+                ''', (mountain,))
+    visited = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    last_visited_list = []
+    for v in visited:
+        last_visited_list.append({
+            "mountain": v[0],
+            "datetime": v[1],
+        })
+    return jsonify(last_visited_list)
 
 
 @app.route('/insert', methods=['POST'])
 def insert_data():
     data = request.json
-    name = data.get('name')
-    surname = data.get('surname')
-    age = data.get('age')
+    mountain = data.get('mountain')
+    altitude = data.get('altitude')
+    country = data.get('country')
+    datetime = data.get('datetime')
 
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('INSERT INTO temp_table (name, surname, age) VALUES (%s, %s, %s)',
-                (name, surname, age))
+    cur.execute('INSERT INTO visited (mountain, altitude, country, datetime) VALUES (%s, %s, %s, %s)',
+                (mountain, altitude, country, datetime))
     conn.commit()
     cur.close()
     conn.close()
 
     return jsonify({'message': 'Data inserted successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
